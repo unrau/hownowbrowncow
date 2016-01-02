ce09775301136b51f76a6af66dd1f50889514201
Chloe Unrau <chloe.unrau@gmail.com>
2016-01-01 21:49:36 -0800

    Initialize the CRUD functionality for Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,44 @@
 class ReviewsController < ApplicationController
 
-  def new
+  def index
+    @reviews = Review.all
   end
-  
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      redirect_to @review
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @review = Review.find(params[:id])
+
+    if @review.update(review_params)
+      redirect_to @review
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:title, :text)
+    end
+
 end
