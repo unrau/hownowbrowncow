f172eaf07036533e60d3a36e68de08f0f14a99d3
Chloe Unrau <chloe.unrau@gmail.com>
2016-01-01 22:08:12 -0800

    Add Destroy functionality for Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -36,6 +36,13 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+
+    redirect_to reviews_path
+  end
+
   private
     def review_params
       params.require(:review).permit(:title, :text)
