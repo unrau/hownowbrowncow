97a0afe284cd2696f3eaa7f04d09ab68192a7540
Chloe Unrau <chloe.unrau@gmail.com>
2016-01-09 09:59:55 -0800

    Add before_action authentication to the Reviews Controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :edit, :create, :update, :destroy]
+  before_action :admin_user,     only: [:new, :edit, :create, :update, :destroy]
 
   def index
     @reviews = Review.all
