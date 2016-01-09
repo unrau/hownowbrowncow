e8cb2184bec91f0e76068e512c943e586317148c
Chloe Unrau <chloe.unrau@gmail.com>
2016-01-09 10:30:24 -0800

    Establish authorization rules for the Recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,5 @@
 class RecipesController < ApplicationController
+  before_action :logged_in_user, only: [:new, :edit, :create, :update, :destroy]
+  before_action :admin_user,     only: [:new, :edit, :create, :update, :destroy]
+  
 end
