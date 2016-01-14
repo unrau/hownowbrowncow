4768be6e163fde44fac010fe62d7fc225a77ba6c
Chloe Unrau <chloe.unrau@gmail.com>
2016-01-14 15:35:30 -0800

    Correct indentation of private controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,24 +5,24 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
-  end
 
-  # Confirms the correct user.
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
-  end
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 
-  # Confirms an admin user.
-  def admin_user
-    redirect_to(root_url) unless is_admin?(current_user)
-  end
-  
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless is_admin?(current_user)
+    end
+
 end
