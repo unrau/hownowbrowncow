e4caea7120c53aa46b73d8f8d347f7d3c1fc3531
Chloe Unrau <chloe.unrau@gmail.com>
2016-01-18 21:57:42 -0800

    Change the default admin email address in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Create the default admin user account
 User.create!(name:                  "Ceres",
-             email:                 "ceres@unrau.me",
+             email:                 "browncow@unrau.me",
              password:              "foobar",
              password_confirmation: "foobar",
              admin:                 true)
