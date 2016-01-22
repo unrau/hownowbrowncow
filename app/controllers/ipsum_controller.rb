a62fb0cee65d6908bca1798dacd1d0cf21ab07cd
Chloe Unrau <chloe.unrau@gmail.com>
2016-01-22 12:26:13 -0800

    Refactor ipsum generator for comprehension

--- app/controllers/ipsum_controller.rb
+++ app/controllers/ipsum_controller.rb
@@ -26,10 +26,10 @@ class IpsumController < ApplicationController
 
   private
 
-    def generate_ipsum(paragraphs, length, start_with_default = true)
+    def generate_ipsum(number_of_paragraphs, paragraph_length, start_with_default_text = true)
       # Generate HTML paragraphs from listed words chosen at random
       #
-      # Version 1.21
+      # Version 1.22
       # Created by Ceres Unrau for Chocolate Milk Ipsum
       # Feel free to use with citation. If you use it, I'd love to know <3 ceres@unrau.me
       #
@@ -39,11 +39,18 @@ class IpsumController < ApplicationController
       # 'long'
       #
 
-      # Options
-      max_paragraph_num = 20
+      # DEVELOPER OPTIONS
+      # Set the maximum number of paragraphs that can be generated
+      max_number_of_paragraphs        = 20
+      # Set the default text string
+      default_text_string = "Chocolate milk"
+      # Set the base number of sentences for each valid length
+      min_sentences_for_short_length  = 2
+      min_sentences_for_medium_length = 5
+      min_sentences_for_long_length   = 10
 
       # This is the main list of words to be placed randomly into the paragraphs
-      ipsum_words = [
+      common_words = [
         'chocolate',
         'milk',
         'brown',
@@ -89,35 +96,40 @@ class IpsumController < ApplicationController
         'like'
       ]
 
+      # Define a method that returns a random word from a given list
+      def return_random_word_from_list(list)
+        return list[Random.rand(list.size)].to_s
+      end
+
       # Set the minimum number of sentences per paragraph based on the length parameter
       length_min = 0
-      case length.downcase
+      case paragraph_length.downcase
       when 'short'
-        length_min = 2
+        length_min = min_sentences_for_short_length
       when 'medium'
-        length_min = 5
+        length_min = min_sentences_for_medium_length
       when 'long'
-        length_min = 10
+        length_min = min_sentences_for_long_length
       else
-        length_min = 5
+        length_min = min_sentences_for_medium_length
       end
 
       # Initialize the ipsum text
       ipsum = '<p>'
 
-      # Allow choice of whether ipsum starts with default text
-      if start_with_default
-        ipsum.concat 'Chocolate milk ipsum'
+      # Start with default text
+      if start_with_default_text
+        ipsum.concat default_text_string
       end
 
       # Limit the amount of paragraphs to the maximum
-      if paragraphs > max_paragraph_num
-        paragraphs = max_paragraph_num
-        ipsum = "<center><p style='font-size:0.7em;' class='col-red'>Result has been limited to #{max_paragraph_num} paragraphs.<em></em></p></center>#{ipsum}"
+      if number_of_paragraphs > max_number_of_paragraphs
+        number_of_paragraphs = max_number_of_paragraphs
+        ipsum = "<center><p style='font-size:0.7em;' class='col-red'>Result has been limited to #{max_number_of_paragraphs} paragraphs.<em></em></p></center>#{ipsum}"
       end
 
       p_count = 0
-      for paragraph in (1..paragraphs)
+      for paragraph in (1..number_of_paragraphs)
         # Initialize a new paragraph
         par = ''
 
@@ -131,26 +143,28 @@ class IpsumController < ApplicationController
             # Initialize a new word
             new_word = ''
 
+            # Capture the last word
+            last_word = par.split.last.to_s
+
             # A random word is seldom selected from the rare word list
             if Random.rand(50) == 1
-              new_word = rare_words[Random.rand(rare_words.size)]
+              new_word = return_random_word_from_list(rare_words)
             # A random word is sometimes selected from the natural sentence word list,
             # as long as the previous word is not one of these words
-            elsif Random.rand(10) == 1 && !natural_words.include?(par.split.last)
-              new_word = natural_words[Random.rand(natural_words.size)]
+            elsif Random.rand(10) == 1 && !natural_words.include?(last_word.downcase)
+              new_word = return_random_word_from_list(natural_words)
             # A random word is selected from the main word list
             else
-              new_word = ipsum_words[Random.rand(ipsum_words.size)]
+              new_word = return_random_word_from_list(common_words)
             end
 
             # Eliminate duplicate consecutive words
-            last_word = par.split.last.to_s
             while new_word.downcase == last_word.downcase
-              new_word = ipsum_words[Random.rand(ipsum_words.size)]
+              new_word = return_random_word_from_list(common_words)
             end
 
             # First word after the optional default text starts with a space and is not capitalized
-            if p_count == 0 && s_count == 0 && w_count == 0 && start_with_default
+            if p_count == 0 && s_count == 0 && w_count == 0 && start_with_default_text
               new_word = ' ' + new_word
             # First word of a normal paragraph doesn't start with a space and is capitalized
             elsif p_count != 0 && s_count == 0 && w_count == 0
@@ -165,7 +179,7 @@ class IpsumController < ApplicationController
 
             # Words can sometimes be followed by a comma to simulate a typcial sentence structure
             # Words in the natural sentence list cannot be followed by a comma.
-            if Random.rand(10) == 1 && !natural_words.include?(new_word.strip)
+            if Random.rand(10) == 1 && !natural_words.include?(new_word.strip.downcase)
               new_word.concat ','
             end
 
